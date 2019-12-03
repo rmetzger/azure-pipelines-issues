53395fdc1e382e1e5b6bd08703b1df4653f33e14
Robert Metzger <rmetzger@apache.org>
2019-12-03 23:04:03 +0800

    finish script

--- deploy-agents.sh
+++ deploy-agents.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 if [[ -z "${AZURE_PAT}" ]]; then
 	echo "Env variable AZURE_PAT not set"
 	exit 1;
@@ -15,7 +17,32 @@ if [[ -z "${NUM_AGENTS}" ]]; then
 	exit 1;
 fi
 
+if [[ -z "${NAME}" ]]; then
+        echo "Env variable NAME not set"
+        exit 1;
+fi
+
 for i in `seq 1 $NUM_AGENTS`
 do
-   echo "Deploying agent1"
+   echo "Creating user agent$i"
+   useradd agent$i
+   sudo usermod -aG docker agent$i
+   cd /home/agent$i
+   mkdir myagent && cd myagent
+
+   echo "Downloading agent tool"
+   wget https://vstsagentpackage.azureedge.net/agent/2.160.1/vsts-agent-linux-x64-2.160.1.tar.gz
+   tar xf vsts-agent-linux-x64-2.160.1.tar.gz
+ 
+   chown -R agent$i /home/agent$i
+
+   echo "setup the agent"
+   ./bin/installdependencies.sh
+   sudo -u agent$i VSTS_AGENT_INPUT_URL=$AZURE_SERVER \
+   VSTS_AGENT_INPUT_AUTH=pat \
+   VSTS_AGENT_INPUT_TOKEN=$AZURE_PAT \
+   VSTS_AGENT_INPUT_POOL=Default \
+   VSTS_AGENT_INPUT_AGENT=$NAME-agent$i \
+   ./config.sh --unattended
+   ./svc.sh install
 done
